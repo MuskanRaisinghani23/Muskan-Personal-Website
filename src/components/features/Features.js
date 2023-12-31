50197ca2decca940ac3f61c0950dfa1b3aeb2795
Muskan Raisinghani <raisinghani.m@northeastern.edu>
2023-12-30 19:57:26 -0500

    What i do section modified. Links added for work.

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -20,36 +20,42 @@ const Features = () => {
           des="With a wealth of experience, I skillfully design databases, with structured and unstructured data, 
           and optimizing systems for seamless operations."
           icon={<FaDatabase />}
+          link="https://github.com/MuskanRaisinghani23/CloudCampus"
         />
         <Card
           title="Data Governance"
           des="I implement robust data governance strategies, ensuring security, cost, and performance optimization 
           throughout the data lifecycle."
           icon={<AiFillSetting />}
+          link="https://www.ltimindtree.com/polarsled"
         />
         <Card
-          title="Development"
+          title="Product Development"
           des="
           I drive product development through a blend of innovative solutions, collaborative leadership, 
           and hands-on experience, ensuring the effortless delivery of high-impact projects."
           icon={<SiProgress />}
+          link="https://www.ltimindtree.com/polarsled"
         />
         <Card
           title="Data Science"
           des="I leverage data science skills to extract meaningful insights, 
           drive informed decision-making, and optimize processes for impactful results."
           icon={<FaBrain />}
+          link="https://github.com/MuskanRaisinghani23/CreditCardApprovalPrediction"
         />
         <Card
           title="Data Pipelining"
           des="I craft end-to-end data pipelines, utilizing a diverse tech stack including Python, Apache Airflow, 
           Cassandra, and Kafka, streamlining data flow for efficient and scalable solutions."
           icon={<SiApacheairflow />}
+          link="https://github.com/MuskanRaisinghani23/Real-Time-Data-Streaming"
         />
         <Card
           title="Web Scraping"
           des="I proficiently employ web scraping techniques, like Beautiful Soup, and Scrapy to extract and synthesize valuable data."
           icon={<FaGlobe />}
+          link="https://github.com/MuskanRaisinghani23/british-airways-customer-feedback-analysis"
         />
       </div>
     </section>
