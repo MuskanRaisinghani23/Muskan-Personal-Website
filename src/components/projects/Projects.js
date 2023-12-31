8afcb7932875fbf603c8ce68a42a10749e6434ba
Muskan Raisinghani <raisinghani.m@northeastern.edu>
2023-12-30 19:43:33 -0500

    Project images updated. Project description added. Project section completed. Database design skill modified

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -20,48 +20,43 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
           title="PolarSled Govern"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Developed and deployed PolarSled Govern Framework - A governance platform providing a 360-degree view of Snowflake. I served as lead developer for two PolarSled accelerators - Sentry and Daily Briefing."
           src={projectOne}
           icon={<FaGlobe/>}
           link="https://www.ltimindtree.com/polarsled/"
         />
         <ProjectsCard
           title="Real time Data Streaming"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="The project demonstrates an end-to-end data pipeline using Apache Kafka to fetch and stream data from a website. The project is containerized with Docker for streamlined deployment and dependency management."
           src={projectTwo}
           icon={<BsGithub />}
           link="https://github.com/MuskanRaisinghani23/Real-Time-Data-Streaming"
         />
         <ProjectsCard
           title="Cloud Campus"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="
+          CloudCampus is a Decentralized Virtual Education Platform designed to enhance the education system and provide equal opportunities for students. The project is implemented using Java Swing and JDBC."
           src={projectThree}
           icon={<BsGithub />}
           link="https://github.com/MuskanRaisinghani23/CloudCampus"
         />
         <ProjectsCard
           title="Credit Card Approval Prediction System"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="The Credit Card Approval Prediction system leverages regression models, AutoML, SHAP analysis, and advanced data visualization techniques. This comprehensive system showcases a blend of sophisticated modeling, automation, and interpretability."
           src={projectFour}
           icon={<BsGithub />}
           link="https://github.com/MuskanRaisinghani23/CreditCardApprovalPrediction"
         />
         <ProjectsCard
           title="Investment Analysis and Portfolio Management"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="This data analytics project uses python's robust modules including Pandas, NumPy, Matplotlib, Seaborn, and Statistics, working extensively with real world Finance data. The project determines powerful relationships between risk, return, and price."
           src={projectFive}
           icon={<BsGithub />}
           link="https://github.com/MuskanRaisinghani23/Investment-Analysis-and-Portfolio-Management"
         />
         <ProjectsCard
           title="Volunteer Management System"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="The project functions as a worldwide platform that facilitates communication between volunteers and organizations in need. Constructed using Java in NetBeans, the goal of the project is to build centralized system for Volunteers."
           src={projectSix}
           icon={<BsGithub />}
           link="https://github.com/MuskanRaisinghani23/VolunteerManagement"
@@ -70,9 +65,11 @@ const Projects = () => {
 
       <div className="flex justify-center items-center text-center mt-20">
         <a href='https://github.com/MuskanRaisinghani23' target='_blank'>
-          <Title
-            title="View more"
-          />
+        <div className="flex flex-col gap-4 font-titleFont">
+          <h3 className="text-sm uppercase font-light text-designColor tracking-wide">
+            <u>VIEW MORE</u>
+          </h3>
+        </div>
         </a>
       </div>
     </section>
--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -158,15 +158,15 @@ const Skills = () => {
             </span>
           </div>
           <div className="overflow-x-hidden">
-            <p className="text-sm uppercase font-medium">Database Design</p>
+            <p className="text-sm uppercase font-medium">Flask</p>
             <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
               <motion.span
                 initial={{ x: "-100%", opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ duration: 0.5, delay: 0.5 }}
-                className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
               >
-                <span className="absolute -top-7 right-0">80%</span>
+                <span className="absolute -top-7 right-0">90%</span>
               </motion.span>
             </span>
           </div>
@@ -177,9 +177,9 @@ const Skills = () => {
                 initial={{ x: "-100%", opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ duration: 0.5, delay: 0.5 }}
-                className="w-[95%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                className="w-[85%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
               >
-                <span className="absolute -top-7 right-0">95%</span>
+                <span className="absolute -top-7 right-0">85%</span>
               </motion.span>
             </span>
           </div>
