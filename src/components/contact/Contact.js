7c1d93f9ab3877a9580da9823f0ce842cadb2000
Muskan Raisinghani <raisinghani.m@northeastern.edu>
2024-01-21 21:06:34 -0500

    Email service added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,9 @@
-import React,{useState} from 'react'
+import React,{useState} from 'react';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import Title from '../layouts/Title';
 import ContactLeft from './ContactLeft';
+import { YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, YOUR_PUBLIC_KEY } from './email_config';
 
 const Contact = () => {
   const [username, setUsername] = useState("");
@@ -11,38 +14,52 @@ const Contact = () => {
   const [errMsg, setErrMsg] = useState("");
   const [successMsg, setSuccessMsg] = useState("");
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    
+  }
+
   // ========== Email Validation start here ==============
-  const emailValidation = () => {
-    return String(email)
-      .toLocaleLowerCase()
-      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
-  };
+  // const emailValidation = () => {
+  //   return String(email)
+  //     .toLocaleLowerCase()
+  //     .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+  // };
   // ========== Email Validation end here ================
 
   const handleSend = (e) => {
     e.preventDefault();
     if (username === "") {
       setErrMsg("Username is required!");
-    } else if (phoneNumber === "") {
-      setErrMsg("Phone number is required!");
+    // } else if (phoneNumber === "") {
+    //   setErrMsg("Phone number is required!");
     } else if (email === "") {
       setErrMsg("Please give your Email!");
-    } else if (!emailValidation(email)) {
-      setErrMsg("Give a valid Email!");
+    // } else if (!emailValidation(email)) {
+    //   setErrMsg("Give a valid Email!");
     } else if (subject === "") {
       setErrMsg("Plese give your Subject!");
     } else if (message === "") {
       setErrMsg("Message is required!");
     } else {
-      setSuccessMsg(
-        `Thank you ${username}, Your message has been sent successfully!`
-      );
-      setErrMsg("");
-      setUsername("");
-      setPhoneNumber("");
-      setEmail("");
-      setSubject("");
-      setMessage("");
+      e.preventDefault();
+      emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form.current, YOUR_PUBLIC_KEY)
+      .then((result) => {
+        setSuccessMsg(
+          `Thank you ${username}, Your message has been sent successfully!`
+        );
+        setErrMsg("");
+        setUsername("");
+        setPhoneNumber("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
+        console.log(result.text);
+      }, (error) => {
+        setErrMsg("Error!");
+      });
+      
     }
   };
   return (
@@ -57,7 +74,7 @@ const Contact = () => {
         <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
           <ContactLeft />
           <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
-            <form className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5">
+            <form className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5" ref={form} onSubmit={sendEmail}>
               {errMsg && (
                 <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
                   {errMsg}
@@ -81,9 +98,10 @@ const Contact = () => {
                       "outline-designColor"
                     } contactInput`}
                     type="text"
+                    name="username"
                   />
                 </div>
-                <div className="w-full lgl:w-1/2 flex flex-col gap-4">
+                {/* <div className="w-full lgl:w-1/2 flex flex-col gap-4">
                   <p className="text-sm text-gray-400 uppercase tracking-wide">
                     Phone Number
                   </p>
@@ -96,7 +114,7 @@ const Contact = () => {
                     } contactInput`}
                     type="text"
                   />
-                </div>
+                </div> */}
               </div>
               <div className="flex flex-col gap-4">
                 <p className="text-sm text-gray-400 uppercase tracking-wide">
@@ -110,6 +128,7 @@ const Contact = () => {
                     "outline-designColor"
                   } contactInput`}
                   type="email"
+                  name="email"
                 />
               </div>
               <div className="flex flex-col gap-4">
@@ -124,6 +143,7 @@ const Contact = () => {
                     "outline-designColor"
                   } contactInput`}
                   type="text"
+                  name="subject"
                 />
               </div>
               <div className="flex flex-col gap-4">
@@ -138,15 +158,16 @@ const Contact = () => {
                   } contactTextArea`}
                   cols="30"
                   rows="8"
+                  name="message"
                 ></textarea>
               </div>
               <div className="w-full">
-                <button
+                <input type="submit"
                   onClick={handleSend}
+                  name='submit'
+                  value="Send Message"
                   className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
-                >
-                  Send Message
-                </button>
+                />
               </div>
               {errMsg && (
                 <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
